251752b5a865bf4a202dd32fd8abece443dfd600
BogdanWDK <pletea66@gmail.com>
2014-12-24 03:59:52 +0200

    Update #6
    IP Command

--- src/config.php
+++ src/config.php
@@ -1,7 +1,7 @@
 <?php
 $admin[] = "em";
 
-$pubfnc = array("whois", "ping", "host","b64_encode","b64_decode","md5_encode","sha1_encode","short","passwd_generate","_banned");
+$pubfnc = array("whois", "ping", "ip","host","b64_encode","b64_decode","md5_encode","sha1_encode","short","passwd_generate","_banned");
 $privfnc = array("sh", "msg","_ban","_unban");
 # Configure chat user and pass
 
